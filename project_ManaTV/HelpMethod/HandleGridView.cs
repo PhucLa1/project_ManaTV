3d78cecc9b13fe46e982931025db22786187219a
Admin <tutquan456@gmail.com>
2023-09-21 22:57:32 +0700

    test

--- project_ManaTV/HelpMethod/HandleGridView.cs
+++ project_ManaTV/HelpMethod/HandleGridView.cs
@@ -14,6 +14,7 @@ namespace project_ManaTV.HelpMethod
         {
             for (int i = 0; i < countCol; i++)
             {
+                int a = 0;
                 gridView.Columns[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 gridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             }
